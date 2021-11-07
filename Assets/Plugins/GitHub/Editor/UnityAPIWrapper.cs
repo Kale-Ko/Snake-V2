3259bde90f531f3a823af4159e335257d7204556
Kale-Ko <github@kaleko.ga>
2021-11-06 20:24:00 -0400

    Update

---
+++ Assets/Plugins/GitHub/Editor/UnityAPIWrapper.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+
+namespace GitHub.Unity
+{
+    [InitializeOnLoad]
+    public class UnityAPIWrapper : ScriptableSingleton<UnityAPIWrapper>
+    {
+        static UnityAPIWrapper()
+        {
+#if UNITY_2018_2_OR_NEWER
+            Editor.finishedDefaultHeaderGUI += editor =>
+            {
+                UnityShim.Raise_Editor_finishedDefaultHeaderGUI(editor);
+            };
+#endif
+        }
+    }
+}
